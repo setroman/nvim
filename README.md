f50e14b02c0f064b4e3c5090a2f28e6961c716d4
Jorge <setroman78@gmail.com>
2025-02-12 19:39:39 -0300

    Agrego configuración básica

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # nvim
 Mi configuración personal del editor nvim
+
+## Configuración básica
+
+- Numeración relativa.
+- Indentado de 2 espacios de ancho con autoindentado en la línea nueva.
+- *wrap* desactivado.
+- Las busquedas ignoran las mayusculas de las minusculas por defecto, pero si en la busqueda se incluyen ambas, no se ignora la diferencia.
+- Resalta la línea conde se encuentra el cursor.
+- *termguicolors* habilitado. Con *background* en *dark* y columna de simbolos.
+- División de pantala hacia la derecha o hacia abajo.
+- *swapfie* desacticado.
---
+++ lua/config/settings.lua
@@ -0,0 +1,43 @@
+
+-- ************************************ 
+-- ***	MIS CONFIGURACIONES BÁSICAS ***
+-- ************************************
+local opt = vim.o
+
+-- Mostrar filas numeradas
+opt.number = true
+
+-- Muestra los números de las filas de manera relativa
+opt.relativenumber = true
+
+-- tabs & indentación (sangría)
+opt.tabstop = 2 -- 2 espacios por tabs (prettier default)
+opt.shiftwidth = 2 -- 2 espacios para el ancho de indentación 
+opt.expandtab = true -- expande tab a espacios
+opt.autoindent = true -- copia la sangria de la línea actual al comenzar una nueva
+
+opt.wrap = false -- false: Los reglones se extienden más alla del limite de la ventana. true: Si el reglon supera el límite de la ventana, se extiente en el reglon siguiente
+
+-- configuracion de búsqueda
+opt.ignorecase = true -- ignora mayúsculas al reluzar búsquedas
+opt.smartcase = true -- si se incluyen mayusculas y minúsculas en la búsqueda, pasa a modo sendible entre mayusculas y minusculas
+
+opt.cursorline = true -- resalta la línea del cursor
+
+-- (el terminal debe soportar true-colors)
+opt.termguicolors = true -- activa colores del terminal para el tema de colores (colorschemes)
+opt.background = "dark" -- colorschemes  que pueden ser claros u oscuro se oscureceran
+opt.signcolumn = "yes" -- muestra una columna de símbolos
+
+-- backspace
+-- opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+-- opt.clipboard:append("unnamedplus") -- registro de clipboard por defecto
+
+-- split windows
+opt.splitright = true -- divide la ventana vertical hacia la derecha
+opt.splitbelow = true -- divide la ventana horizontal hacia abajo
+
+-- turn off swapfile
+opt.swapfile = false -- deshabilita el archivo de intercambio
