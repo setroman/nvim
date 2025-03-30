e9d4d9292c45913d278770e3c1c53b3b3f83fb6c
Jorge <setroman78@gmail.com>
2025-03-30 20:31:11 -0300

    Correji un problemita de marckdown y actualice unos paquetes

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,11 +2,12 @@
 -- ***  Extensión  de previsualización de archivos markdown ***
 -- ************************************************************
 
+-- return {}
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+    ft = { "markdown" },
 }
 
 --[[ ## Errata
